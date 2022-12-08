ccf5d8d44842f60658ef243ca454b73a5341966a
acimovpr <acimovpr@chamberlain.com>
2022-12-08 10:58:57 -0600

    Updated day1

--- day1.cpp
+++ day1.cpp
@@ -1,16 +1,57 @@
 #include <iostream>
-#include <fstream
-usingnamespace std;
+#include <fstream>
+using namespace std;
 
 int main()
 {
 
-    int sum;
-    int max;
+    int sum = 0;
+    int max = 0;
 
-    int buff;
+    ifstream file;
+    string filename = "day1.txt";
 
+    int line = 0;
+    int value_in_line;
 
+    file.open(filename);
+
+    if(file.fail())
+    {
+        cout << "file failed to open" << endl;
+        return 1;
+    }
+
+    while(!file.eof())
+    {
+
+        value_in_line = getline(file, line);
+
+        if(!isdigit(value_in_line))
+        {
+            if(max < sum)
+            {
+                max = sum;
+            }
+            sum = 0;
+        }
+        else
+        {
+            sum += value_in_line;
+        }
+
+        line++;
+    }
+
+    if(max < sum)
+    {
+        max = sum;
+    }
+    sum = 0;
+
+    cout << max << endl;
+
+    file.close();
 
     return 0;
 }
