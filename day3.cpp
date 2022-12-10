54c8b4795ed11718b5de252bc1b0d8f8554ccc8c
pvacimovic <99547816+pvacimovic@users.noreply.github.com>
2022-12-09 20:07:35 -0600

    Finished day3 in linear time

--- day3.cpp
+++ day3.cpp
@@ -1,42 +1,70 @@
-#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <cctype>
 using namespace std;
 
-char compare(string first, string second, int len)
-{
-    char t = 'A';
-
-    for(int i=0; i<len; i++){
-        for(int j=0; j<len; j++){
-            if(first[i]==second[j]){
-                t = first[i];
-            }
-        }
-    }
-    return t;
-}
+char compare(string, string, int);
 
 int main() {
 
     ifstream file;
     file.open("day3.txt");
+    int b = 1;
 
     string line = "";
     int length;
-    int j = 1;
+    string strings[3];
 
     char target;
     int points;
     int sum=0;
+
+    int sum2=0;
 
     if (file.fail()) {
         cout << "file failed to open" << endl;
         return 1;
     }
 
+    // part2
+    while (!file.eof()) {
+
+        int mat[3][52];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 52; j++) {
+            mat[i][j] = 0;
+            }
+        }       
+
+        for(int i=0; i<3; i++){
+
+            getline(file, line);
+
+            strings[i] = line;
+
+            for(int j=0; j<strings[i].length(); j++){
+
+                if(islower(strings[i][j])){
+                    mat[i][(static_cast<int>(strings[i][j])-97)] = 1;
+                }
+                else{
+                    mat[i][(static_cast<int>(strings[i][j])-39)] = 1;
+                }
+            }
+
+        }
+
+        for(int i=0; i<52; i++){
+            if(mat[0][i]==1 && mat[0][i]==mat[1][i] && mat[0][i]==mat[2][i]){
+                // cout << b++ << ": " << i << endl;
+                sum2 += (i+1);
+            }
+        }
+    }
+
+    file.seekg(0); // move to the beggining to a file
+
+    // part 1
     while (!file.eof()) {
 
         getline(file, line);
@@ -58,16 +86,29 @@ int main() {
         }
 
         sum += points;
-        cout << j << " " << line << " = " << firstHalf << " + " << secondHalf << " -- " << target << ": " << points << endl;
-        j++;
+        // cout << b++ << " " << line << " = " << firstHalf << " + " << secondHalf << " -- " << target << ": " << points << endl;
 
         line = "";
-        target = NULL;
     }
 
     file.close();
 
-    cout << "Sum of priorities: " << sum << endl;
+    cout << "Part 1 - Sum of priorities: " << sum << endl;
+    cout << "Part 2 - Sum of priorities: " << sum2 << endl;
 
     return 0;
+}
+
+char compare(string first, string second, int len)
+{
+    char t = 'A';
+
+    for(int i=0; i<len; i++){
+        for(int j=0; j<len; j++){
+            if(first[i]==second[j]){
+                t = first[i];
+            }
+        }
+    }
+    return t;
 }
